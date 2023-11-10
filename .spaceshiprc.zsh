ef01851199c722ba410f34b2eee7c50d437f52ad
Jason Victor <36286598+Theqwertypusher@users.noreply.github.com>
2023-11-10 15:25:08 -0700

    Create .spaceshiprc.zsh

---
+++ .spaceshiprc.zsh
@@ -0,0 +1,9 @@
+# Display time
+SPACESHIP_TIME_SHOW=true
+SPACESHIP_TIME_FORMAT='%D{%H:%M}'
+
+# Display username always
+SPACESHIP_USER_SHOW=false
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
